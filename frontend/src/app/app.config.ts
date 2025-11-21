ec41cc712cf791b241ffa99db3ba7da8ce46748e
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2025-11-21 01:59:58 +0100

    chore(frontend): enable HttpClient fetch backend for SSR via withFetch()

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import {
   provideClientHydration,
   withEventReplay,
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
   ],
 };
